7c012c9dbcaae8dce4af7e68134c5a30638b6c0c
Alex Benko <benkoalex521@gmail.com>
2020-04-08 16:53:53 -0700

    Example profile data to start small

---
+++ src/example/exampleProfileData.js
@@ -0,0 +1,11 @@
+var exampleProfileData = {
+  "id": "JxRLg70NObxwRs_b6GzMF2su7FVlwECmDHmQA9Jn1R35R78",
+  "accountId": "rkSPBxI-wKDpBkmNOChlCbDUzE76Zwt_wJLsSERFMSjYEuU",
+  "puuid": "BShf1hGyliJ2SnvMILst9IqAAcK7uQBtg0VqDzYfGMcZg3A9YU4W6CJvqSoZPaQGJ4H0znUgaPAXOg",
+  "name": "Daddy Thick",
+  "profileIconId": 4512,
+  "revisionDate": 1586372311000,
+  "summonerLevel": 191
+}
+
+export default exampleProfileData
