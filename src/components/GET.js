61c2b6f65fb5c26132835b2881ead234b5c75fcd
Alex Benko <benkoalex521@gmail.com>
2020-04-09 16:50:39 -0700

    Fetch request is now in a seperate file and now changes based on user input

--- src/components/GET.js
+++ src/components/GET.js
@@ -1,5 +1,25 @@
-var searchForProfile = ({search,key,max=5}) => {
+var searchForProfile = ({search,key},cb) => {
+  //only way i was able to access Riot api was using this proxy
+    //https://github.com/Rob--W/cors-anywhere/
+    var proxy = 'https://cors-anywhere.herokuapp.com/';
+    var target = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${search}?api_key=${key}`;
 
+    fetch(proxy + target)
+     .then(res => res.json())
+     .then(
+       (result) =>{
+         console.log('Sucess!')
+         console.log(result)
+         if(cb){
+           cb(result)
+         }
+
+       },
+
+       (fail) =>{
+        console.log(fail);
+       }
+    )
 }
 
 export default searchForProfile;
