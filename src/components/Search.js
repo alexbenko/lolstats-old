842d3669b11f49f125c5c6635930157f05ee3e12
Alex Benko <benkoalex521@gmail.com>
2020-04-09 16:53:54 -0700

    Search is now a class and is able to handle user input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,37 @@
 import React from 'react';
-var Search = (props) =>(
-  <div className="search">
-  <input type="text" placeholder="Search For Summoner.."/>
-  </div>
-)
+
+class Search extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      search: ''
+    };
+  }
+
+  handleInputChange(search){
+    console.log(search)
+    this.props.handleSearchChange(search.target.value);
+    this.setState({
+      value: search.target.value
+    })
+  }
+  render(){
+    return (
+
+    <div className="search">
+      <input type="text" placeholder="Search For Summoner.." value={this.state.value} onChange={this.handleInputChange.bind(this)}/>
+    </div>
+    )
+
+  }
+
+
+
+
+}
+
+
 
 
 export default Search ;
