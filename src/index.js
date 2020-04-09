976422f95164811e8e07fd4227e97bf1018c7a25
Alex Benko <benkoalex521@gmail.com>
2020-04-09 16:52:12 -0700

    Searching Riot's api and api key are now passed in in this file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
+import RIOT_API_KEY from './config/rito.js';
+import searchForProfile from './components/GET.js'
 
 
-ReactDOM.render(<App />,document.getElementById('app'));
+
+ReactDOM.render(<App searchForProfile={searchForProfile} RIOT_API_KEY={RIOT_API_KEY}/>,document.getElementById('app'));
 
