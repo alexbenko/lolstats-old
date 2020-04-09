8400123e970a79fb0214244ffa74acaddcb1c991
Alex Benko <benkoalex521@gmail.com>
2020-04-08 21:43:29 -0700

    progress on fetching data from Riot Api

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,33 @@ import '../styles/App.css';
 import exampleProfileData from '../example/exampleProfileData';
 import Profile from './Profile.js'
 import Search from './Search.js'
+import RIOT_API_KEY from '../config/rito.js';
 
 class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      currentProfile: exampleProfileData[0]
+      loaded:false,
+      currentProfile: exampleProfileData
     }
+  }
+
+  componentDidMount() {
+    fetch(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Daddy%20Thick?api_key=${RIOT_API_KEY}`)
+     .then(res => res.json())
+     .then(
+       (result) =>{
+         this.setState({
+          loaded:true,
+          currentProfile: result
+         });
+       },
+
+       (fail) =>{
+         console.log(fail);
+        console.log("Failed!")
+       }
+    )
   }
 
 
