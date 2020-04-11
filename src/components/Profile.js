422cbd5a89678f88f8185b786f8442d8c37a552c
Alex Benko <benkoalex521@gmail.com>
2020-04-10 21:10:50 -0700

    Put it in its own div tag

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 var Profile = (props) => (
 
-<div className="prof">
-  <h3>Summoner:  {props.profile.name}</h3>
-  <h3>{`Level: ${props.profile.summonerLevel}`}</h3>
+<div>
+  <div className="prof">
+    <h3>{`Summoner: ${props.profile.name}`}</h3>
+    <h3>{`Level: ${props.profile.summonerLevel}`}</h3>
+  </div>
 </div>
-
 );
 
 
