363cd891907841dc3faaa0413c3072fe9746bd9d
Alex Benko <benkoalex521@gmail.com>
2020-04-10 21:10:22 -0700

    Added a new tag

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../styles/App.css';
-import exampleProfileData from '../example/exampleProfileData';
 import Profile from './Profile.js'
 import Search from './Search.js'
+import MostPlayedChamps from './mostPlayedChamps.js';
 import loading from '../images/teemo_dance.gif'
-import RIOT_API_KEY from '../config/rito.js';
+import exampleProfileData from '../example/exampleProfileData'
+
 //right now only works with NA accounts
 //can set up a drop down menu for user to select region
 
@@ -12,40 +13,29 @@ class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
+      firstSearch:true,
       loaded:false,
       currentProfile: exampleProfileData
     }
   }
 
   componentDidMount() {
-    //only way i was able to access Riot api was using this proxy
-    //https://github.com/Rob--W/cors-anywhere/
-    var proxy = 'https://cors-anywhere.herokuapp.com/';
-    var target = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Sneaky?api_key=${RIOT_API_KEY}`;
 
-    fetch(/*proxy + target*/)
-     .then(res => res.json())
-     .then(
-       (result) =>{
-         console.log('Sucess!')
-         console.log(result)
-         this.setState({
-           loaded:true,
-           currentProfile: result
-         });
-       },
-
-       (fail) =>{
-        console.log(fail);
-         this.setState({
-           loaded:false
-         })
-
-
-       }
-    )
   }
 
+  getProfile(search){
+    let searchObj = {
+      key : this.props.RIOT_API_KEY,
+      search: search
+    }
+    this.props.searchForProfile (searchObj, (profile) =>
+      this.setState({
+        firstSearch:false,
+        loaded:true,
+        currentProfile: profile
+      })
+    );
+  }
 
   render() {
 
@@ -56,27 +46,34 @@ class App extends React.Component {
       return (
         <div className="loading">
           <h1>LOLStats</h1>
+
           <nav className="nav">
-          <Search />
+           <Search handleSearchChange={this.getProfile.bind(this)}/>
            </nav>
+
            <h1>Loading...</h1>
            <img src={loading} alt="" />
         </div>
       )
     }
 
-    return (
-      <div>
-        <h1>LOLStats</h1>
-        <nav className="nav">
-          <Search />
-        </nav>
 
-        <div className ='prof'>
-          <Profile profile={this.state.currentProfile} />
+      return (
+        <div>
+          <h1>LOLStats</h1>
+          <nav className="nav">
+            <Search handleSearchChange={this.getProfile.bind(this)} />
+          </nav>
+
+          <div className ='prof'>
+            <Profile profile={this.state.currentProfile} />
+            <MostPlayedChamps />
+          </div>
+
         </div>
-      </div>
-    );
+      );
+
+
   };
 
 
