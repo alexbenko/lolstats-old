e8ee780b7fd6a6631909402aa5c24435a0a7a556
Alex Benko <benkoalex521@gmail.com>
2020-04-11 14:38:52 -0700

    mostplatedchamps now correctly displays five most displayed champIds using example data

--- src/components/mostPlayedChamps.js
+++ src/components/mostPlayedChamps.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-var MostPlayedChamps = (props) => (
-  <div>
-    <h3>Most Played Champs</h3>
-  </div>
-);
+var MostPlayedChamps = ({champs}) => {
+  return (
+  <div className="everything">
+    <div className="champTitle">
+      <h3> Five Most Played Champs</h3>
+    </div>
+
+    <div className="champList">
+      {champs.map((champ,index) => (
+
+      <h3>{champ["championId"]}</h3>
+
+
+
+      ))}
+    </div>
+
+   </div>
+  );
+};
 
 export default MostPlayedChamps;
