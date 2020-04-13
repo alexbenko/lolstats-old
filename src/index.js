a776dbfd09f0dcdd08e5fd302ae33047527c7c3b
Alex Benko <benkoalex521@gmail.com>
2020-04-12 17:18:24 -0700

    Imported champ fetcher

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import RIOT_API_KEY from './config/rito.js';
-import searchForProfile from './components/GET.js'
+import searchForProfile from './components/searchProfile.js';
+import searchForChamps from './components/searchChamps.js';
 
 
 
