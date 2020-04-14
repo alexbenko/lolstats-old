207dc7b1e6d8dae3b4e55c44b6a47e7ab2c76033
Alex Benko <benkoalex521@gmail.com>
2020-04-13 20:35:55 -0700

    Fetch request for the champion data

--- src/components/searchChamps.js
+++ src/components/searchChamps.js
@@ -1,6 +1,28 @@
 //get request that needs accounts encrypted ID, and my API key
 var searchForChampions = ({encryptedId,key}, cb) =>{
 
+  var proxy = 'https://cors-anywhere.herokuapp.com/';
+  var target = `https://na1.api.riotgames.com/lol/champion-mastery/v4/scores/by-summoner/${encryptedId}?api_key=${key}`;
+
+  fetch(proxy + target)
+  .then(res => res.json())
+  .then(
+    (result) =>{
+
+      if(cb){
+        console.log('Sucess!');
+        console.log(result);
+        cb(result);
+      }
+
+    },
+
+    (fail) =>{
+
+     console.log(fail);
+    }
+ )
 }
+
 
 export default searchForChampions
