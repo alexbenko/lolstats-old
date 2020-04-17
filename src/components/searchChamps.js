a5d0c180827226debcf11e3afb49e1f5d851fe79
Alex Benko <benkoalex521@gmail.com>
2020-04-16 22:25:17 -0700

    tried moving some statements around

--- src/components/searchChamps.js
+++ src/components/searchChamps.js
@@ -1,9 +1,9 @@
 //get request that needs accounts encrypted ID, and my API key
 var searchForChampions = ({encryptedId,key}, cb) =>{
-
+  //change the state inside the get request
   var proxy = 'https://cors-anywhere.herokuapp.com/';
   var target = `https://na1.api.riotgames.com/lol/champion-mastery/v4/scores/by-summoner/${encryptedId}?api_key=${key}`;
-
+  console.log("Fetch:" ,encryptedId);
   fetch(proxy + target)
   .then(res => res.json())
   .then(
