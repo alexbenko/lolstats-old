2fa8526493415f7cae5c50d4b166343c70d5c746
Alex Benko <benkoalex521@gmail.com>
2020-04-17 21:34:22 -0700

    Rewrote the get request using axios

--- src/components/searchProfile.js
+++ src/components/searchProfile.js
@@ -1,22 +1,23 @@
+import axios from 'axios';
+
 var searchForProfile = ({search,key},cb) => {
   //only way i was able to access Riot api was using this proxy
     //https://github.com/Rob--W/cors-anywhere/
     var proxy = 'https://cors-anywhere.herokuapp.com/';
     var target = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${search}?api_key=${key}`;
 
-    fetch(proxy + target)
-     .then(res => res.json())
-     .then(
-       (result) =>{
-          console.log('Sucess!');
-          cb(result);
-       },
+    axios.get(proxy + target)
+      .then(res =>{
+        console.log(res.data);
+        cb(res.data);
+      })
+      .catch((error) => {
+        if(error.response){
+          console.log(error.response.data);
+          console.log(error.response.response);
+        }
+      })
 
-       (fail) =>{
-
-        console.log('Search For Profile Failed:' , fail);
-       }
-    )
 }
 
 //var searchForChampions({})
